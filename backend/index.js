4b12c5e5bab343293385e821e07010e2b8732835
Kanishk Singh <kanishk.singh198@gmail.com>
2023-03-31 11:38:45 +0530

    Added: express session, error handling

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
 const mongoose = require("mongoose");
+const expressError = require("./utils/expressError");
 
 const app = express();
 const PORT = 4000;
@@ -16,6 +18,13 @@ const passport = require("passport");
 
 app.use(express.json());
 app.use(cors()); // allow cross origin resource sharing between frontend and backend
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 app.use(passport.initialize());
 
 // configure passport
@@ -35,6 +44,17 @@ connection.once("open", function () {
 // routes
 app.use("/campgrounds", campgroundRoutes);
 app.use("/", userRoutes);
+
+// Error handling middlewares
+app.all("*", (req, res, next) => {
+	next(new expressError("Error 404, Not found", 404));
+});
+app.use((err, req, res, next) => {
+	if (!err.message) err.message = "Oh no, something went wrong. :(";
+	if (!err.status) err.status = 500;
+
+	res.status(err.status).send("Some error occurred");
+});
 
 app.listen(PORT, function () {
 	console.log("App listening on port " + PORT);
